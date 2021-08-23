220cdec81cefac0c16c295f7dd115ff8dd0a03ec
sakshi swati <83575830+sakshi-swati@users.noreply.github.com>
2021-08-23 13:30:56 +0530

    Add files via upload

---
+++ project 2/src/App.js
@@ -0,0 +1,53 @@
+import Users from "./components/user";
+import './App.css';
+
+import React, { Component } from 'react'
+	
+class App extends Component {
+  constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {users_data :[],
+                loading : true                
+  }
+  
+	// Binding this keyword
+	this.updateState = this.updateState.bind(this)
+  }
+    
+  updateState(){
+      console.log("VT");
+      document.getElementById("main").style.display='flex';
+      const link="https://reqres.in/api/users?page=1";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+      <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <h3 class="title">HAPPY <p className="para">Load user card</p></h3>
+            
+            <button onClick={this.updateState}>Load Users</button>
+          </div>
+        </nav>
+      <Users loading={this.state.loading} users={this.state.users_data}/>
+    </>
+    )
+  }
+}
+	
+export default App;
+
